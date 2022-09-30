fb0724e1c01d63506fa5af707d6ab3a3e74b23c7
kirkade <mookj@mail.ru>
2022-09-30 12:47:06 +0300

    first commit microtask2 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+        ]
+    )
+
+    let [title,setTitle] = useState('')
+
+    const addMessage = (title:string) => {
+        let newMessage = {message:title}
+        setMessage([newMessage,...message])
+
+    }
+
+    const callbackButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
+    }
+
+    return (
+        <div className={'App'}>
+            <div>
+                <Input setTitle={setTitle} title={title}/>
+                <Button name={'+'} callback={callbackButtonHandler}/>
+            </div>
+            <div>
+                {message.map((el,index)=>{
+                    return (
+                        <div key={index}>{el.message}</div>
+                    )
+                })}
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
